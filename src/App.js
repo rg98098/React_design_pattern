9284930262fefca9c738d1b2f93175d8b1a748c7
rg98098 <rg98098@gmail.com>
2022-11-20 15:15:22 +0530

    base project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import './App.css'
-import SplitScreen from "./SplitScreen";
-
-const LeftHandComponent =({name})=> <h1 style={{background:"green"}}>{name}</h1>
-const RightHandComponent =({message})=> <h1 style={{background:"red"}}>{message}</h1>
 
 function App() {
   return <div className="App">
-  <SplitScreen
-    leftWeight={1}
-    rightWeight={3}
-  >
-  <LeftHandComponent name={"Rahul"}/>
-  <RightHandComponent message={"Hello"}/>
-    </SplitScreen>
+  Hello
   </div>;
 }
 
