d7bc989610c838660a453725577d1e8603c1b3c7
rg98098 <rg98098@gmail.com>
2022-11-20 14:50:58 +0530

    split screen component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import "./App.css";
+import './App.css'
+import SplitScreen from "./SplitScreen";
+
+const LeftHandComponent =()=> <h1 style={{background:"green"}}>Left</h1>
+const RightHandComponent =()=> <h1 style={{background:"red"}}>Right</h1>
 
 function App() {
-  return <div className="App">Hello App</div>;
+  return <div className="App">
+  <SplitScreen
+    left={LeftHandComponent}
+    right={RightHandComponent}
+    leftWeight={1}
+    rightWeight={3}
+  /></div>;
 }
 
 export default App;
---
+++ src/SplitScreen.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div `
+  display: flex;
+`;
+
+const Pane = styled.div `
+  flex: ${props => props.weight};
+`
+
+const SplitScreen = ({left:Left,right:Right,leftWeight =1, rightWeight=1}) => {
+  return (
+    <Container>
+      <Pane weight={leftWeight}><Left/></Pane>
+      <Pane weight={rightWeight}><Right/></Pane>
+    </Container>
+  );
+};
+
+export default SplitScreen;
